ecc0f4c710782065abd3a06964c387f16076ab46
s-take <s-take@nec.com>
2022-04-07 10:00:40 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CenteredTabsPage from "./components/CenterdTabsPage";
+import Typography from "@mui/material/Typography";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: `"Hiragino Kaku Gothic ProN", "ヒラギノ角ゴ ProN", "Hiragino Sans", "ヒラギノ角ゴシック", "Meiryo", "メイリオ", sans-serif`,
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            誰かに何かを話してもらうアプリ
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <CenteredTabsPage />
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/CenterdTabsPage.tsx
@@ -0,0 +1,249 @@
+import * as React from "react";
+import { useRef, useState } from "react";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Button, FormControl, Grid, IconButton, TextField, Typography } from "@mui/material";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemButton from "@mui/material/ListItemButton";
+import Checkbox from "@mui/material/Checkbox";
+import shuffle from "lodash.shuffle";
+import useSound from "use-sound";
+import RollSound from "./roll.mp3";
+import StopSound from "./stop.mp3";
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
+
+const talkTitles = [
+  "最近嬉しかったこと",
+  "最近買ったモノ",
+  "最近見たYoutube動画",
+  "最近の趣味",
+  "今一番欲しいもの",
+  "週末に出かけた場所",
+  "昨日の夜ご飯",
+  "好きな漫画・アニメ",
+  "小学校の頃の夢",
+  "出身地について",
+];
+
+export default function CenteredTabsPage() {
+  const [value, setValue] = useState("1");
+  const [userName, setUserName] = useState("");
+  const [personList, setPersonList] = useState<string[]>([]);
+  const inputRef = useRef(null);
+  const [inputError, setInputError] = useState(false);
+  const [inputErrorText, setInputErrorText] = useState("");
+  const [shuffledUser, setShuffledUser] = useState("");
+  const [shuffledTitle, setShuffledTitle] = useState("");
+
+  const [checked, setChecked] = React.useState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+  const [playRollSound, { sound }] = useSound(RollSound);
+  const [playStopSound] = useSound(StopSound);
+  const { width, height } = useWindowSize();
+  const [confetti, setConfetti] = useState(false);
+
+  //functions
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+  const handleUserNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(event.target.value);
+  };
+
+  const handlePersonAddEvent = () => {
+    if (userName === "") {
+      setInputError(true);
+      setInputErrorText("入力必須です");
+      return;
+    } else if (personList.findIndex((name) => name === userName) !== -1) {
+      setInputError(true);
+      setInputErrorText("同じ名前が存在します");
+      return;
+    }
+    setPersonList([...personList, userName]);
+    setUserName("");
+    setInputError(false);
+    setInputErrorText("");
+  };
+
+  const handleDeletePerson = (index: number) => {
+    const newPersonList = [...personList];
+    newPersonList.splice(index, 1);
+    setPersonList(newPersonList);
+  };
+
+  const handleToggle = (value: number) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  const handleMatch = async () => {
+    if (personList.length === 0) {
+      alert("参加者がいません");
+      return;
+    } else if (checked.length === 0) {
+      alert("トークテーマが選択されていません");
+      return;
+    }
+    setConfetti(false);
+    playRollSound();
+    for (let i = 0; i < 20; i++) {
+      setShuffledUser(shuffle(personList)[0]);
+      setShuffledTitle(talkTitles[shuffle(checked)[0]]);
+      await new Promise((resolve) => setTimeout(resolve, 500));
+    }
+    playStopSound();
+    setConfetti(true);
+  };
+
+  return (
+    <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <TabList onChange={handleChange} centered>
+            <Tab value="1" label="参加者" />
+            <Tab value="2" label="トークテーマ" />
+            <Tab value="3" label="シャッフル" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">
+          <Grid container spacing={2} direction="column" alignItems="center">
+            <Grid item xs={12}>
+              <FormControl fullWidth>
+                <TextField
+                  id="inputPerson"
+                  label="参加者"
+                  variant="outlined"
+                  value={userName}
+                  onChange={handleUserNameChange}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter" && e.keyCode === 13) {
+                      handlePersonAddEvent();
+                    }
+                  }}
+                  error={inputError}
+                  inputRef={inputRef}
+                  helperText={inputErrorText}
+                  InputProps={{
+                    endAdornment: (
+                      <IconButton
+                        color="primary"
+                        aria-label="upload picture"
+                        component="span"
+                        size="large"
+                        edge="start"
+                        onClick={handlePersonAddEvent}
+                      >
+                        <PersonAddAlt1Icon />
+                      </IconButton>
+                    ),
+                  }}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <List>
+                {personList.map((value, i) => {
+                  return (
+                    <ListItem
+                      key={value}
+                      divider={true}
+                      secondaryAction={
+                        <IconButton edge="end" aria-label="delete" onClick={() => handleDeletePerson(i)}>
+                          <DeleteIcon />
+                        </IconButton>
+                      }
+                    >
+                      <ListItemText
+                        primary={value}
+                        primaryTypographyProps={{
+                          width: "160px",
+                        }}
+                      />
+                    </ListItem>
+                  );
+                })}
+              </List>
+            </Grid>
+          </Grid>
+        </TabPanel>
+        <TabPanel value="2">
+          <Grid container spacing={2} direction="column" alignItems="center">
+            <Grid item xs={12}>
+              <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+                {talkTitles.map((value, index) => {
+                  const labelId = `checkbox-list-label-${value}`;
+
+                  return (
+                    <ListItem
+                      key={value}
+                      secondaryAction={<IconButton edge="end" aria-label="comments"></IconButton>}
+                      disablePadding
+                    >
+                      <ListItemButton role={undefined} onClick={handleToggle(index)} dense>
+                        <ListItemIcon>
+                          <Checkbox
+                            edge="start"
+                            checked={checked.indexOf(index) !== -1}
+                            tabIndex={-1}
+                            disableRipple
+                            inputProps={{ "aria-labelledby": labelId }}
+                          />
+                        </ListItemIcon>
+                        <ListItemText id={labelId} primary={value} />
+                      </ListItemButton>
+                    </ListItem>
+                  );
+                })}
+              </List>
+            </Grid>
+          </Grid>
+        </TabPanel>
+        <TabPanel value="3">
+          <Grid container spacing={2} direction="column" alignItems="center">
+            <Grid item xs={12}>
+              <Button variant="contained" onClick={handleMatch}>
+                GO!!
+              </Button>
+            </Grid>
+            <Box mt={10} />
+            <Grid item xs={12}>
+              <Typography variant="h3" gutterBottom component="div">
+                {shuffledUser} さん
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h4" gutterBottom component="div">
+                の
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h3" gutterBottom component="div">
+                {shuffledTitle}
+              </Typography>
+            </Grid>
+          </Grid>
+        </TabPanel>
+      </TabContext>
+      {confetti && <Confetti width={width} height={height} recycle={false} />}
+    </Box>
+  );
+}
